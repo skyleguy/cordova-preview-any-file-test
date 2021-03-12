a7422aa7fa39b391587279932eee0118df2edc4c
Kyle Lobsinger <kyle.lobsinger@fmr.com>
2021-03-12 15:54:24 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Preview';
+
+  public ngOnInit(): void {
+    console.log('starting timeout');
+    setTimeout((): void => {
+      console.log('starting preview');
+      (<any>window).PreviewAnyFile.previewAsset(
+        (win: any) => console.log("open status",win),
+        (error: any) => console.error("open failed", error),
+        '/assets/Budgeting.xlsx'
+    );
+    }, 5000);
+  }
 }
